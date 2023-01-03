451fa55136b38ec172d0f4ae6bb22a2c6e34196b
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2023-01-03 12:10:39 +0200

    update app ts

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import FavoritePage from "./pages/FavoritePage";
 import HomePage from "./pages/HomePage";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/favorite" element={<FavoritePage />} />
-    </Routes>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favorite" element={<FavoritePage />} />
+      </Routes>
+    </>
   );
 }
 
